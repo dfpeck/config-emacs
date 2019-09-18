9daac0431900e3699190c47dec4c51bea2e76746
dfpeck <dakota.f.peck@protonmail.com>
2019-09-18 11:06:27 -0600

    Added hooks for irony mode

--- my-c.el
+++ my-c.el
@@ -3,5 +3,11 @@
   ;; prevent c-mode from overriding my-keys
   (define-key c-mode-keymap (kbd "C-d") nil))
 
+;; irony mode ;;
+(add-hook 'c++-mode-hook 'irony-mode)
+(add-hook 'c-mode-hook 'irony-mode)
+(add-hook 'objc-mode-hook 'irony-mode)
+(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+
 ;; Formatting ;;
 (setq-default c-block-comment-prefix "* ")
