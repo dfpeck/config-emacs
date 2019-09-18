f9bb3acbdbed660e6889bb5717121ca796923c2e
dfpeck <dakota.f.peck@protonmail.com>
2019-09-18 11:11:07 -0600

    Added load instructions for company irony

--- my-c.el
+++ my-c.el
@@ -3,11 +3,15 @@
   ;; prevent c-mode from overriding my-keys
   (define-key c-mode-keymap (kbd "C-d") nil))
 
-;; irony mode ;;
+;; Irony Mode ;;
 (add-hook 'c++-mode-hook 'irony-mode)
 (add-hook 'c-mode-hook 'irony-mode)
 (add-hook 'objc-mode-hook 'irony-mode)
 (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+
+;; Company Irony ;;
+(with-eval-after-load "company"
+  (add-to-list 'company-backends 'company-irony))
 
 ;; Formatting ;;
 (setq-default c-block-comment-prefix "* ")
