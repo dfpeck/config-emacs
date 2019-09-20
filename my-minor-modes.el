45c080c80dec298f5e3b67d97e322231d99b625f
dfpeck <dakota.f.peck@protonmail.com>
2019-09-20 14:44:42 -0600

    Adjusted hide/show mode keybinds

--- my-minor-modes.el
+++ my-minor-modes.el
@@ -1,3 +1,8 @@
 ;; Hide/Show Minor Mode ;;
 (with-eval-after-load "hideshow"
-  (define-key hs-minor-mode-map (kbd "C-c @ @") 'hs-toggle-hiding)) ; previously NOTHING
+  (define-key hs-minor-mode-map (kbd "C-c 2 2") 'hs-toggle-hiding) ; previously NOTHING
+  (define-key hs-minor-mode-map (kbd "C-c 2 h") 'hs-hide-block)    ; previously NOTHING
+  (define-key hs-minor-mode-map (kbd "C-c 2 s") 'hs-show-block)    ; previously NOTHING
+  (define-key hs-minor-mode-map (kbd "C-c 2 L") 'hs-hide-level)    ; previously NOTHING
+  (define-key hs-minor-mode-map (kbd "C-c 2 H") 'hs-hide-all)      ; previously NOTHING
+  (define-key hs-minor-mode-map (kbd "C-c 2 S") 'hs-show-all))     ; previously NOTHING
