082e0d22a5296c107b31b9263adc004f467548ec
dfpeck <dakota.f.peck@protonmail.com>
2019-10-01 17:50:38 -0600

    Added keybind for show-paren-mode

--- my-keys.el
+++ my-keys.el
@@ -43,6 +43,7 @@
 (define-key my-keys-minor-mode-map (kbd "C-c m a") 'auto-fill-mode)        ; previously NIL
 (define-key my-keys-minor-mode-map (kbd "C-c m m") 'menu-bar-mode)         ; previously undo
 (define-key my-keys-minor-mode-map (kbd "C-c m s") 'subword-mode)          ; previously NIL
+(define-key my-keys-minor-mode-map (kbd "C-c m p") 'show-paren-mode)       ; previously NIL
 
 ;; Miscellaneous ;;
                                         ; (define-key my-keys-minor-mode-map (kbd "C-c q") (lambda () (interactive) (insert "?")))
