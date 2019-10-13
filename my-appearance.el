39a59ba8f9287e6413f27c28b90467b3122a2312
dfpeck <dakota.f.peck@protonmail.com>
2019-10-13 14:37:07 -0600

    Cleaned up appearance settings

--- my-appearance.el
+++ my-appearance.el
@@ -1,41 +1,16 @@
-;; Adjusting Fonts ;;
-(defun dfp/fonts-cjk-japanese ()
-  (interactive)
-  (set-fontset-font "fontset-default" 'han "TakaoExGothic"))
-;; (defun dfp/fonts-cjk-chinese ()
-;;   (interactive)
-;;   (set-fontset-font "fontset-default" 'han ))
-
 ;; Monokai ;;
-;; (setf custom-enabled-themes 'monokai)
-;; (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-;; (load-theme 'monokai t)
 (load-file "~/.emacs.d/themes/monokai-theme.el")
 
 ;; Cursor Color ;;
-(setq default-frame-alist '((cursor-color . "white")))
+(add-to-list 'default-frame-alist '((cursor-color . "white")))
 
 ;; Transparency ;;
-(dfp/set-default-frame-alpha (car dfp/base-frame-alphas))
+(dfp/set-default-frame-alpha (car dfp/quick-alphas))
 
 ;; Text-Appearance ;;
 (setq-default fill-column 80)
 (setq-default tab-width 4)
-(set-fontset-font "fontset-default" 'kana "TakaoExGothic")
-(let ((font-size
-       ;; Use font size 9 on Windows
-       (if (string-match-p (regexp-quote "mingw") (emacs-version)) "9"
-         ;; Use font size 10 elsewhere
-         "10.5")))
-  (add-to-list 'default-frame-alist `(font . ,(concat "DejaVu Sans Mono-" font-size))))
-(dfp/fonts-cjk-japanese)
 
-;; Scroll Bars ;;
-(defun dfp/disable-scroll-bars (frame)
-  (modify-frame-parameters frame
-                           '((vertical-scroll-bars . nil)
-                             (horizontal-scroll-bars . nil))))
-(add-hook 'after-make-frame-functions 'dfp/disable-scroll-bars)
-
-;; Comment Sytle ;;
-;; (setq comment-style 'multi-line)
+;; Scroll Bars
+(add-to-list 'default-frame-alist '(vertical-scroll-bars . nil))
+(add-to-list 'default-frame-alist '(horizontal-scroll-bars . nil))
