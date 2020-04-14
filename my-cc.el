f5007abe05d74a674830ef1814ad64a4a6e220b8
dfpeck <dakota.f.peck@protonmail.com>
2020-04-14 12:16:12 -0600

    Stopped disabling electric comments and added ricoh-ascii c-style

--- my-cc.el
+++ my-cc.el
@@ -1,5 +1,13 @@
 ;; Keybindings ;;
-(with-eval-after-load "cc-mode"
-  ;; disable electric comments
-  (define-key c-mode-base-map "/" 'self-insert-command)
-  (define-key c-mode-base-map "*" 'self-insert-command))
+;; (with-eval-after-load "cc-mode"
+;;   ;; disable electric comments
+;;   (define-key c-mode-base-map "/" 'self-insert-command)
+;;   (define-key c-mode-base-map "*" 'self-insert-command))
+
+(c-add-style "ricoh-ascii"
+             '("bsd"
+               (c-basic-offset . 2)
+               (c-offsets-alist
+                (substatement-open . 0)
+                (inline-open       . 0)
+                (case-label        . +))))
